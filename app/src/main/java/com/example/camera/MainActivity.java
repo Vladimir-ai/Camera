6f7641183b70374e69c84c2224946fc4be167ff0
Vladimir-ai <bespalvovan42@gmail.com>
2020-10-17 15:05:38 +0300

    Initial

--- app/src/main/java/com/example/camera/MainActivity.java
+++ app/src/main/java/com/example/camera/MainActivity.java
@@ -1,32 +1,6 @@
 package com.example.camera;
 
-import android.Manifest;
-import android.content.Context;
-import android.content.pm.PackageManager;
-import android.graphics.ImageFormat;
-import android.graphics.SurfaceTexture;
-import android.hardware.camera2.CameraAccessException;
-import android.hardware.camera2.CameraCaptureSession;
-import android.hardware.camera2.CameraDevice;
-import android.hardware.camera2.CameraManager;
-import android.hardware.camera2.CaptureRequest;
-import android.hardware.camera2.TotalCaptureResult;
-import android.media.Image;
-import android.media.ImageReader;
-import android.os.Bundle;
-import android.os.Environment;
-import android.util.Log;
-import android.view.Surface;
-import android.view.View;
-
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.nio.ByteBuffer;
-import java.util.Arrays;
 
 
 public class MainActivity extends AppCompatActivity {
